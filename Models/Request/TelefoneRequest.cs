22cbd40e2f10aeea952e2fa31fa002749ab932c3
Paulo Eduardo Correia <correia97@gmail.com>
2023-08-31 13:15:30 -0300

    separação de projetos

---
+++ Models/Request/TelefoneRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Cadastro.MVC.Models.Request
+{
+    public class TelefoneRequest
+    {
+        public int Id { get; set; }
+        public string DDI { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ Models/Response/TelefoneResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Cadastro.MVC.Models.Response
+{
+    public class TelefoneResponse
+    {
+        public int Id { get; set; }
+        public string DDI { get; set; }
+        public string Telefone { get; set; }
+    }
+}
